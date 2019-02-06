3d813ff9ef76d07a8eb1dbd7f2ca06efb64d28f0
lukedrum <32622061+lukedrum@users.noreply.github.com>
2019-02-06 12:42:26 +0100

    Update xls_to_suJSON.py
    Change way of taking subject scores

--- xls_to_suJSON.py
+++ xls_to_suJSON.py
@@ -3,51 +3,71 @@ import json
 import numpy as np
 
 
-def default(o):                                                       # resolve numpy type problem
+# Resolve numpy type problem
+def default(o):
     if isinstance(o, np.int64):
         return int(o)
     raise TypeError
 
 
-def write_to_json_file(path, file_name, data):                        # write to json file function
+def write_to_json_file(path, file_name, data):
+    """
+    Function which allows saving to JSON file
+
+    :param path: Define result file path
+    :param file_name: File name
+    :param data: Data to save
+    :return:
+    """
     file_path_name_wext = './' + path + './' + file_name + '.json'
     with open(file_path_name_wext, 'w') as fp:
         json.dump(data, fp, indent=2, default=default)
 
 
-with open('config.json') as experiment_description:                   # open configuration file
-    ed = json.load(experiment_description)
+# Open configuration file
+with open('config.json') as configuration_file:
+    cf = json.load(configuration_file)
 
-wb = pd.read_excel(ed['file_name'],                                   # open xls file with data
-                   sheet_name=ed['sheet_hdr_name'],
-                   header=ed['header_row_pos'])
+# Open xls file with data
+wb = pd.read_excel(cf['file_name'],
+                   sheet_name=cf['sheet_hdr_name'],
+                   header=cf['header_row_pos'])
 
-src_num = wb[ed['src_hdr_name']].unique()                             # create list of src
-hrc_num = wb[ed['hrc_hdr_name']].unique()                             # create list of hrc
-pvs = wb[ed['file_hdr_name']].unique()                                # create list of pvs
+# Create list of unique src
+src_num = wb[cf['src_hdr_name']].unique()
 
-path = './'                                                           # define result file path
-file_name = 'result'                                                  # define result file name
+# Create list of unique hrc
+hrc_num = wb[cf['hrc_hdr_name']].unique()
+
+# Create list of unique pvs
+pvs = wb[cf['file_hdr_name']].unique()
+
+# Define result file path
+path = cf['result_file_path']
+
+# Define result file name
+file_name = cf['result_file_name']
 
 # DATASET_NAME
-dataset_name = ed['dataset_name']
+dataset_name = cf['dataset_name']
 
 # SUJSON_VERSION
 sujson_version = '1.1-in_progress'
 
 # CHARACTERISTICS
-characteristics = ed['characteristics']
+characteristics = cf['characteristics']
 
 # TASKS
-tasks = ed['tasks']
+tasks = cf['tasks']
 
 # SCALES
-scales = ed['scales']
+scales = cf['scales']
 
 # QUESTIONS
-questions = ed['questions']
+questions = cf['questions']
 
-final_data = {'dataset_name': dataset_name,                           # define structure of final json file
+# Define structure of final json file
+final_data = {'dataset_name': dataset_name,
               'sujson_version': sujson_version,
               'characteristics': characteristics,
               'tasks': tasks,
@@ -62,78 +82,92 @@ final_data = {'dataset_name': dataset_name,                           # define s
 
 # SRC
 id_num = 1
-for i in src_num:                                                     # iterate over every element from src_num list
+for i in src_num:
     if i <= 9:
         a = '0' + str(i)
     else:
         a = str(i)
-    final_data['src'].append({'id': id_num,                           # add to final data list of src
+    # Add to final data list of src
+    final_data['src'].append({'id': id_num,
                               'name': dataset_name + '_src' + a})
     id_num = id_num + 1
 
 # HRC
 id_num = 1
-for i in hrc_num:                                                     # iterate over every element from hrc_num list
+for i in hrc_num:
     if i <= 9:
         a = '0' + str(i)
     else:
         a = str(i)
-    final_data['hrc'].append({'id': id_num,                           # add to final data list of hrc
+    # Add to final data list of hrc
+    final_data['hrc'].append({'id': id_num,
                               'name': dataset_name + '_hrc' + a})
     id_num = id_num + 1
 
 # PVS
 id_num = 1
 m = 0
-for i in src_num:                                                     # iterate over every element from src_num list
+for i in src_num:
     if i <= 9:
         a = '0' + str(i)
     else:
         a = str(i)
-    src_id = final_data['src'][m]['id']                               # take id of m element from src list
+    # Take id of m element from src list
+    src_id = final_data['src'][m]['id']
     m = m + 1
     n = 0
-    for j in hrc_num:                                                 # iterate over every element from hrc_num list
+    for j in hrc_num:
         if j <= 9:
             b = '0' + str(j)
         else:
             b = str(j)
-        hrc_id = final_data['hrc'][n]['id']                           # take id of n element from hrc list
+        # Take id of n element from hrc list
+        hrc_id = final_data['hrc'][n]['id']
         n = n + 1
-        for element in pvs:                                           # iterate over every element from pvs list
+        for element in pvs:
             valid_name = dataset_name + '_src' + a + '_hrc' + b
-            if element.startswith(valid_name):                        # compare created name with one from data file
-                final_data['pvs'].append({'id': id_num,               # add to final data list of pvs
+            # Compare created name with one from data file (only for code test purpose)
+            if element.startswith(valid_name):
+                # Add to final data list of pvs
+                final_data['pvs'].append({'id': id_num,
                                           'src_id': src_id,
                                           'hrc_id': hrc_id,
                                           # 'file_check_name': dataset_name + '_src' + a + '_hrc' + b,
                                           'file_name': element})
                 id_num = id_num + 1
 
-# TEST SUBJECTS DIFFERENT
-subject_start_num = ed['scores_range']['start']                       # take header number of first subject
-subject_finish_num = ed['scores_range']['stop']                       # take header number of last subject
+# TEST SUBJECTS
+# Take header number of first subject
+subject_start_num = cf['scores_range']['start']
+
+# Take header number of last subject
+subject_finish_num = cf['scores_range']['stop']
  
 subjects = 1
-for i in range(subject_start_num, subject_finish_num+1):              # iterate over every element in range from config
-    final_data['subjects'].append({'id': subjects})                   # add to final data list of subjects
+for i in range(subject_start_num, subject_finish_num+1):
+    # Add to final data list of subjects
+    final_data['subjects'].append({'id': subjects})
     subjects += 1
 
 # TRIALS
 id_num = 1
 subject_num = 0
-for subject_id in final_data['subjects']:                             # iterate over every element from subjects
-    subject_id_num = final_data['subjects'][subject_num]['id']        # take id from subjects list
+for subject_id in final_data['subjects']:
+    # Take id from subjects list
+    subject_id_num = final_data['subjects'][subject_num]['id']
     subject_num = subject_num + 1
     task_num = 0
-    for task_id in final_data['tasks']:                               # iterate over every element from tasks
-        task_id_num = final_data['tasks'][task_num]['id']             # take id from tasks list
+    for task_id in final_data['tasks']:
+        # Take id from tasks list
+        task_id_num = final_data['tasks'][task_num]['id']
         task_num = task_num + 1
         pvs_num = 0
-        for pvs_id in final_data['pvs']:                              # iterate over every element from pvs
-            pvs_id_num = final_data['pvs'][pvs_num]['id']             # take id from pvs list
+        for pvs_id in final_data['pvs']:
+            # Take id from pvs list
+            pvs_id_num = final_data['pvs'][pvs_num]['id']
             pvs_num = pvs_num + 1
-            final_data['trials'].append({'id': id_num,                # add to final data list of trials
+            # Add to final data list of trials
+            final_data['trials'].append({'id': id_num,
                                          'subject_id': subject_id_num,
                                          'task_id': task_id_num,
                                          'pvs_id': pvs_id_num,
@@ -143,19 +177,26 @@ for subject_id in final_data['subjects']:                             # iterate 
 # SCORES
 id_num = 1
 score_num = 0
-for score_id in final_data['trials']:                                 # iterate over every element from trials
-    pvs_id_num = final_data['trials'][score_num]['pvs_id']            # take pvs id from trials list
-    subject_id_num = final_data['trials'][score_num]['subject_id']    # take subject id from trials list
-    subject_score = wb[subject_id_num][pvs_id_num - 1]
+for score_id in final_data['trials']:
+    # Take pvs id from trials list
+    pvs_id_num = final_data['trials'][score_num]['pvs_id']
+    # Take subject id from trials list
+    subject_id_num = final_data['trials'][score_num]['subject_id']
+    subject_id_num = subject_id_num + subject_start_num - 2
+    # Take subject score from dataframe
+    subject_score = wb[wb.columns[subject_id_num]][pvs_id_num - 1]
     score_num = score_num + 1
     question_num = 0
-    for question_id in final_data['questions']:                       # iterate over every element from questions
-        question_id_num = final_data['questions'][question_num]['id'] # take id from questions list
+    for question_id in final_data['questions']:
+        # Take id from questions list
+        question_id_num = final_data['questions'][question_num]['id']
         question_num = question_num + 1
-        final_data['scores'].append({'id': id_num,                    # add to final data list of scores
+        # Add to final data list of scores
+        final_data['scores'].append({'id': id_num,
                                      'question_id': question_id_num,
                                      'pvs_id': pvs_id_num,
                                      'score': subject_score})
         id_num = id_num + 1
 
-write_to_json_file(path, file_name, final_data)                       # save results to json file
+# Save results to json file
+write_to_json_file(path, file_name, final_data)
