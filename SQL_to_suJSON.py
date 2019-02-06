ac6ae336fbdf9f5660bf3e81e91a82a1a6919d40
Jakub Nawała <jakub.tadeusz.nawala@gmail.com>
2019-02-06 16:07:41 +0100

    Reformat the file and show how to use the db cursor as iterator
    Using the cursor as iterator is much more memory usage safe.

--- SQL_to_suJSON.py
+++ SQL_to_suJSON.py
@@ -2,33 +2,32 @@ import json
 import numpy as np
 import mysql.connector
 
-db = mysql.connector.connect(
+cnx = mysql.connector.connect(
     user='root',
     password='root',
     host='localhost',
     database='sujson')
 
-cursor = db.cursor()
+cursor = cnx.cursor()
 
 
-def default(o):                                                       # resolve numpy type problem
+def default(o):  # resolve numpy type problem
     if isinstance(o, np.int64):
         return int(o)
     # raise TypeError
 
 
-def write_to_json_file(path, file_name, data):                        # write to json file function
+def write_to_json_file(path, file_name, data):  # write to json file function
     file_path_name_wext = './' + path + './' + file_name + '.json'
     with open(file_path_name_wext, 'w') as fp:
         json.dump(data, fp, indent=2, default=default)
 
 
-with open('config.json') as experiment_description:                   # open configuration file
+with open('config.json') as experiment_description:  # open configuration file
     ed = json.load(experiment_description)
 
-
-path = './'                                                           # define result file path
-file_name = 'result'                                                  # define result file name
+path = './'  # define result file path
+file_name = 'result'  # define result file name
 
 # DATASET_NAME
 dataset_name = ed['dataset_name']
@@ -48,7 +47,7 @@ scales = ed['scales']
 # QUESTIONS
 questions = ed['questions']
 
-final_data = {'dataset_name': dataset_name,                           # define structure of final json file
+final_data = {'dataset_name': dataset_name,  # define structure of final json file
               'sujson_version': sujson_version,
               'characteristics': characteristics,
               'tasks': tasks,
@@ -63,8 +62,7 @@ final_data = {'dataset_name': dataset_name,                           # define s
 
 # PVS
 cursor.execute('SELECT ID, FILE_PATH FROM tests_file')
-result = cursor.fetchall()
-for (id, filepath) in result:
+for (id, filepath) in cursor:
     final_data['pvs'].append({'id': id,
                               'src_id': None,
                               'hrc_id': None,
@@ -79,7 +77,7 @@ for id in subjects:
 # TRIALS
 cursor.execute('SELECT ID, ID_USER, ID_FILE, ID_TEST, TEST_DATE FROM results')
 result = cursor.fetchall()
-for (id, subject_id,  pvs_id, test_id, test_date) in result:
+for (id, subject_id, pvs_id, test_id, test_date) in result:
     final_data['trials'].append({'id': id,
                                  'subject_id': subject_id,
                                  'task_id': None,
@@ -90,7 +88,7 @@ for (id, subject_id,  pvs_id, test_id, test_date) in result:
 # SCORES
 cursor.execute('SELECT ID, ID_USER, ID_FILE, ID_TEST, TEST_DATE FROM results')
 result = cursor.fetchall()
-for (id, subject_id,  pvs_id, test_id, test_date) in result:
+for (id, subject_id, pvs_id, test_id, test_date) in result:
     final_data['scores'].append({'id': id,
                                  'subject_id': subject_id,
                                  'task_id': None,
@@ -98,4 +96,4 @@ for (id, subject_id,  pvs_id, test_id, test_date) in result:
                                  'test_id': test_id,
                                  'test_date': test_date})
 
-write_to_json_file(path, file_name, final_data)                       # save results to json file
+write_to_json_file(path, file_name, final_data)  # save results to json file
