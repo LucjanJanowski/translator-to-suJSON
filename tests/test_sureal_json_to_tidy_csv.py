892f837f0d590484c4490552ffef53f8b5a4ee4c
Mateusz Potaśnik <potasnik@student.agh.edu.pl>
2021-02-18 12:49:06 +0100

    Improved tests of conversion

--- tests/test_sureal_json_to_tidy_csv.py
+++ tests/test_sureal_json_to_tidy_csv.py
@@ -1,5 +1,6 @@
 from sujson.Json2csv import Json2csv
 import unittest
+import filecmp
 
 class ConversionJsonToCsv(unittest.TestCase):
     def setUp(self):
@@ -8,6 +9,7 @@ class ConversionJsonToCsv(unittest.TestCase):
     def test_conversion(self):
         self.json_to_csv.load_json("files/Netflix.json")
         self.json_to_csv.convert("files/Netflix_ctest.csv", delimiter=";")
+        assert filecmp.cmp("files/Netflix_ctest.csv", "files/Netflix.csv")
 
 
 if __name__ == '__main__':
--- tests/test_tidy_csv_to_sureal_json.py
+++ tests/test_tidy_csv_to_sureal_json.py
@@ -1,5 +1,6 @@
 from sujson.Csv2json import Csv2json
 import unittest
+import filecmp
 
 class ConvertCsvToJson(unittest.TestCase):
     def setUp(self):
@@ -8,6 +9,7 @@ class ConvertCsvToJson(unittest.TestCase):
     def test_conversion(self):
         self.csv_to_json.load("files/Netflix.csv", delimiter=";")
         self.csv_to_json.convert("files/Netflix_jtest.json")
+        assert filecmp.cmp("files/Netflix_jtest.json", "files/Netflix.json")
 
 
 if __name__ == '__main__':
